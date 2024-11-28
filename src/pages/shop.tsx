502f1d5c8c23481b81395a3e3ef1f185276e15dd
Unai <reikrb@gmail.com>
2024-11-28 08:52:55 +0100

    [ER6-444] ADD - skips auth

---
+++ src/pages/shop.tsx
@@ -0,0 +1,12 @@
+
+export default function Shop() {
+  return (
+    <html lang="es">
+      <body className="bg-cover bg-center" style={{ backgroundImage: 'url(/images/background.jpg)'}}>
+        <div>
+            <p>HELLO WORLD</p>
+        </div>
+      </body>
+    </html>
+  );
+}
