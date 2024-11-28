7cb008c633a058fa959b048be09b61c741c7a20e
Victor06Duran <victor.duran@ikasle.aeg.eus>
2024-11-28 09:12:42 +0100

    [ER6-418]ADD - Kaotika header + access to shop through header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,6 +26,9 @@ const Header: React.FC = () => {
       <Link href="/hall">
         <span className={router.pathname == "/hall" ? "text-4xl mx-6 underline" :"text-4xl mx-6 hover:underline"}>Hall of Fame</span>
       </Link>
+      <Link href="/shop">
+        <span className={router.pathname == "/shop" ? "text-4xl mx-6 underline" :"text-4xl mx-6 hover:underline"}>Aivan's Store</span>
+      </Link>
     </nav>);
   } else {
     navigation = (<nav className="flex-1 text-center">     
@@ -37,6 +40,9 @@ const Header: React.FC = () => {
       </Link>
       <Link href="/hall">
         <span className={router.pathname == "/hall" ? "text-4xl mx-6 underline" :"text-4xl mx-6 hover:underline"}>Hall of Fame</span>
+      </Link>
+      <Link href="/shop">
+        <span className={router.pathname == "/shop" ? "text-4xl mx-6 underline" :"text-4xl mx-6 hover:underline"}>Aivan's Store</span>
       </Link>
     </nav>);
   }
