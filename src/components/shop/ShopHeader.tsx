8b06f2392f83fb15dbd85c1c8d0c55600bf45b11
Victor06Duran <victor.duran@ikasle.aeg.eus>
2024-11-28 13:28:46 +0100

    [ER6-418]MODIFY - Hide drop-down and product categories when sell is selected

--- src/components/shop/ShopHeader.tsx
+++ src/components/shop/ShopHeader.tsx
@@ -5,6 +5,8 @@ const ShopHeader: React.FC = () => {
     const [activeAction, setActiveAction] = useState<string>('buy');
     const [activeCategory, setActiveCategory] = useState<string>('weapons');
     const [shopType, setShopType] = useState<string>('equipment');
+    const [isCartActive, setIsCartActive] = useState(false);
+
 
     const handleCategoryChange = (category: string) => {
         setActiveCategory(category);
@@ -39,7 +41,7 @@ const ShopHeader: React.FC = () => {
 
     return (
         <>
-            <div className=" flex items-center bg-black justify-center py-2">
+            <div className=" flex items-center bg-black justify-center py-1">
                 <Image src="/images/HeaderDivider.webp" alt="HeaderDivider" width={350} height={1} />
             </div>
             <header className="fixed w-full bg-black text-white z-50 shadow-md">
@@ -47,52 +49,55 @@ const ShopHeader: React.FC = () => {
                     <div className="flex items-center">
                         <button
                             onClick={() => handleActionChange('buy')}
-                            className={`text-3xl text-darkSepiapx-1 py-2 hover:underline ${activeAction === 'buy' ? 'underline' : ''}`}
+                            className={`text-3xl px-1 py-2 hover:underline ${activeAction === 'buy' ? 'underline text-yellow-500' : ''}`}
                         >
                             Buy
                         </button>
                         <button
                             onClick={() => handleActionChange('sell')}
-                            className={`text-3xl px-1 py-2 mx-6 hover:underline ${activeAction === 'sell' ? 'underline' : ''}`}
+                            className={`text-3xl px-1 py-2 mx-6 hover:underline ${activeAction === 'sell' ? 'underline text-yellow-500 ' : ''}`}
                         >
                             Sell
                         </button>
                     </div>
 
-                    <div className="flex items-center ">
-                        <span className="text-xl mx-2 block">SHOP</span>
-                        <select
-                            onChange={(e) => handleShopTypeChange(e.target.value)}
-                            value={shopType}
-                            className="block w-full bg-gray-200 text-black border border-gray-800 rounded-md py-1 pl-6 pr-10 text-2xl"
-                        >
-                            <option value="equipment">Equipment</option>
-                            <option value="magical">Magical Stuff</option>
-                        </select>
-                        <nav className="flex items-center space-x-6 ml-6">
-                            {categoriesToDisplay.map((category) => (
-                                <button
-                                    key={category.key}
-                                    onClick={() => handleCategoryChange(category.key)}
-                                    className={`text-3xl px-1 py-2 hover:underline ${activeCategory === category.key ? 'underline' : ''}`}
-                                >
-                                    {category.label}
-                                </button>
-                            ))}
-                        </nav>
-                    </div>
-
+                    {activeAction === 'buy' && (
+                        <div className="flex items-center ">
+                            <span className="text-xl mx-2 block">SHOP</span>
+                            <select
+                                onChange={(e) => handleShopTypeChange(e.target.value)}
+                                value={shopType}
+                                className="block w-full bg-gray-200 text-black border border-gray-800 rounded-md py-1 pl-6 pr-10 text-2xl"
+                            >
+                                <option value="equipment">Equipment</option>
+                                <option value="magical">Magical Stuff</option>
+                            </select>
+                            <nav className="flex items-center space-x-6 ml-6">
+                                {categoriesToDisplay.map((category) => (
+                                    <button
+                                        key={category.key}
+                                        onClick={() => handleCategoryChange(category.key)}
+                                        className={`text-3xl px-1 py-2 hover:underline ${activeCategory === category.key ? 'underline text-medievalSepia' : ''}`}
+                                    >
+                                        {category.label}
+                                    </button>
+                                ))}
+                            </nav>
+                        </div>
+                    )}
                     <div className="flex items-center">
                         <button
                             onClick={() => handleCategoryChange('cart')}
-                            className={`flex items-center text-3xl px-1 py-2 hover:underline ${activeCategory === 'cart' ? 'underline' : ''}`}
+                            className={`flex items-center text-3xl px-1 py-2 hover:underline ${activeCategory === 'cart' ? 'underline text-yellow-500' : ''}`}
                         >
                             <Image src="/images/cart.webp" alt="Cart" width={65} height={65} />
-                            <span className="ml-2">Cart</span>
+                            <span className="ml-2 ">Cart</span>
                         </button>
                     </div>
                 </div>
+
             </header>
+
         </>
     );
 };
