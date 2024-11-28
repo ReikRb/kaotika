242d58881194fc4ee8d38fa412c36d23eeeeedb4
Unai <reikrb@gmail.com>
2024-11-28 13:16:19 +0100

    [ER6-449] ADD - user fetch on shop load

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -1,10 +1,50 @@
+import { Player } from '@/_common/interfaces/Player';
 import Layout from '@/components/Layout';
 import Loading from '@/components/Loading';
 import ShopHeader from '@/components/shop/ShopHeader';
-import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 export default function Shop() {
-  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [player, setPlayer] = useState<Player>();
+  const [loading, setLoading] = useState(true);
+  const [currentEquipment, setCurrentEquipment] = useState({});
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+      if (session?.user?.email) {
+        const fetchPlayerData = async () => {
+          try {
+            setLoading(true);
+            console.log('Fetching user character');
+            const res = await fetch(`/api/shop/player?email=${session.user?.email}`);
+            
+            if (res.status === 200) {
+                const response = await res.json();
+                setCurrentEquipment(response.equipment);
+                console.log('Users character fetch complete:', response)
+                setPlayer(response);
+
+            } else if (res.status === 404) {
+            //   const response = await res.json();
+
+            } else {
+              setError('An error occurred while checking registration');
+            }
+          } catch (error) {
+            setError('An error occurred while checking registration');
+          } finally {
+            setLoading(false);
+          }
+        };
+  
+        fetchPlayerData();
+      }
+    }, [session]);
+  
 	if (loading) {
     return <Loading />;
 	}
