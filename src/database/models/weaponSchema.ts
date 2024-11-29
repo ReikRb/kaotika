2b1f017af9046f64e31510357c9cf3fb3ee134ab
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2024-11-29 08:18:39 +0100

    [ER6-453]ADD - the weapon model

---
+++ src/database/models/weaponSchema.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const weaponSchema = new mongoose.Schema({
+    _id: { type: String },
+    name: { type: String },
+    description: { type: String },
+    type: { type: String },
+    image: { type: String },
+    die_faces: { type: Number },
+    die_modifier: { type: Number },
+    die_num: { type: Number },
+    value: { type: Number },
+    base_percentage: { type: Number },
+    modifiers: {
+        intelligence: { type: Number },
+        dexterity: { type: Number },
+        constitution: { type: Number },
+        insanity: { type: Number },
+        charisma: { type: Number },
+        strength: { type: Number },
+    },
+    min_lvl: { type: Number },
+    isUnique: { type: Boolean },
+    isActive: { type: Boolean },
+});
+
+module.exports = mongoose.models.Weapon || mongoose.model('Weapon', weaponSchema);
