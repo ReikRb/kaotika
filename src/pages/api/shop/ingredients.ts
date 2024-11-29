49c186e1c716081eca9be77e8026224b9a7a900f
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2024-11-29 08:55:54 +0100

    [ER6-446]UPDATE - the ingredient model rute in ingredients.ts

--- src/pages/api/shop/ingredients.ts
+++ src/pages/api/shop/ingredients.ts
@@ -1,16 +1,20 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { Ingredient } from '@/database/models/IngredientSchema';
 import { DBConnect, DBDisconnect } from '@/database/dbHandler';
+import type { NextApiRequest, NextApiResponse } from 'next';
+const Ingredient = require("../../../database/models/ingredientSchema");
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	try {
-		await DBConnect();
-		const ingredients = await Ingredient.find();
-		console.log(ingredients);
-		await DBDisconnect();
-		res.status(200).json(ingredients);
-	} catch (error) {
-		console.error('Failed to get ingredients:', error);
-		res.status(500).json({ error: 'Failed to get ingredients' });
-	}
+    try {
+        await DBConnect()
+        const response = await Ingredient.find();
+        
+        await DBDisconnect()
+        if (response) {
+            return res.status(200).json(response);
+        }else {
+            return res.status(404).json(response);
+        }
+         
+    } catch (error) {
+        return res.status(500).json({ error: 'Internal server error' });
+    }
 }
