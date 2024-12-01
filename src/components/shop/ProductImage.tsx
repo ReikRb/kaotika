3ee7eaeae5bed21a58d9fa7ceac09b2ec118b398
Victor06Duran <victor.duran@ikasle.aeg.eus>
2024-12-01 12:37:16 +0100

    [ER6-471]MODIFY - Product Image component

--- src/components/shop/ProductImage.tsx
+++ src/components/shop/ProductImage.tsx
@@ -7,7 +7,7 @@ interface ProductImageProps {
 
 const ProductImage: React.FC<ProductImageProps> = ({ imageSrc, altText }) => (
     <div className="flex items-center justify-center h-[50%] border-b-2 border-gray-300">
-        <img src={'#'} alt={'a'} className="h-32 w-32" />
+        <img src={imageSrc} alt={altText} className="h-32 w-32" />
     </div>
 );
 
