070c097fe6a35e1ec9ce1b01fb1ddc95ada44c8c
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2024-12-02 09:03:33 +0100

    [ER6-429]UPDATE - the components to send the products by props

--- src/components/shop/ProductsCardsContainer.tsx
+++ src/components/shop/ProductsCardsContainer.tsx
@@ -1,8 +1,29 @@
-const ProductsCardsContainer: React.FC = () => {
+import { Weapon } from "@/_common/interfaces/Weapon";
+import { Helmet } from "@/_common/interfaces/Helmet";
+import { Armor } from "@/_common/interfaces/Armor";
+import { Boot } from "@/_common/interfaces/Boot";
+import { Ring } from "@/_common/interfaces/Ring";
+import { Artifact } from "@/_common/interfaces/Artifact";
+import { Shield } from "@/_common/interfaces/Shield";
+import { Ingredient } from "@/_common/interfaces/Ingredient";
+
+interface Props {
+    products: Weapon[] | Helmet[] | Armor[] | Boot[] | Ring[] | Artifact[] | Shield[] | Ingredient[];
+}
+
+const ProductsCardsContainer: React.FC<Props> = (data) => {
     return (
         <>
-            <div className="w-full row-span-8 row-start-3 flex p-[2%] border-2 border-red-600">
-                
+            <div className="w-full row-span-8 row-start-3 p-[2%] overflow-y-auto border-2 border-red-600">
+                {       
+                    data.products.map((product, i) => {
+                        return (
+                            <div key={i} className="w-full row-span-8 row-start-3 p-[2%]">
+                                <p>{product.name}</p>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </>
     );
--- src/components/shop/RightContainer.tsx
+++ src/components/shop/RightContainer.tsx
@@ -1,10 +1,22 @@
 import ProductsCardsContainer from "./ProductsCardsContainer";
+import { Weapon } from "@/_common/interfaces/Weapon";
+import { Helmet } from "@/_common/interfaces/Helmet";
+import { Armor } from "@/_common/interfaces/Armor";
+import { Boot } from "@/_common/interfaces/Boot";
+import { Ring } from "@/_common/interfaces/Ring";
+import { Artifact } from "@/_common/interfaces/Artifact";
+import { Shield } from "@/_common/interfaces/Shield";
+import { Ingredient } from "@/_common/interfaces/Ingredient";
 
-const RightContainer: React.FC = (products) => {
+interface Props {
+    products: Weapon[] | Helmet[] | Armor[] | Boot[] | Ring[] | Artifact[] | Shield[] | Ingredient[];
+}
+
+const RightContainer: React.FC<Props> = (data) => {
     return (
         <>
             <div className="w-4/12 grid grid-rows-10 border-2 border-red-600">
-                <ProductsCardsContainer/>
+                <ProductsCardsContainer products={data.products!}/>
             </div>
         </>
     );
