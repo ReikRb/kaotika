3e962bc3322abb346c3f844b8622217cec23d354
Victor06Duran <victor.duran@ikasle.aeg.eus>
2024-12-03 13:29:34 +0100

    [ER-476]FIX - Right Panel with proper dimensions

--- src/components/shop/RightSidePanelComponent.tsx
+++ src/components/shop/RightSidePanelComponent.tsx
@@ -14,8 +14,8 @@ const RightSidePanel: React.FC<RightSidePanelProps> = ({ isOpen, togglePanel }) 
 
     return (
         <>
-            <div className={`fixed top-[20%] right-0 h-full bg-black z-50 transition-transform ${isOpen ? 'translate-x-full' : 'translate-x-1/3'}`} style={{ width: '50%' }}>
-                <button onClick={togglePanel} className="absolute top-1/3 left-0">
+            <div className={`fixed top-[20%] right-0 h-[80%] max bg-black border-1 border-sepia z-50 transition-transform ${isOpen ? 'translate-x-full' : 'translate-x-1/3'}`} style={{ width: '50%' }}>
+                <button onClick={togglePanel} className="absolute top-[38%] left-0">
                     <img src={ArrowImage} alt="Close Right Panel" className="h-36 w-9" />
                 </button>
                 <div className="p-4 text-white">
