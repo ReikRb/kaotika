b9b1dd1cd14e18551b087ebe86ffa992af58fa1b
Unai <reikrb@gmail.com>
2024-12-05 10:54:44 +0100

    [ER6-494] UPDATE - refactors Product Card Component && adds his styles to constants.ts

--- src/components/shop/ProductCard.tsx
+++ src/components/shop/ProductCard.tsx
@@ -7,7 +7,7 @@ import { Ring } from "@/_common/interfaces/Ring";
 import { Shield } from "@/_common/interfaces/Shield";
 import { Weapon } from "@/_common/interfaces/Weapon";
 import GoldComponent from "./GoldComponent";
-import { useEffect, useState } from "react";
+import { PRODUCT_CART, PRODUCT_SHOP } from "@/constants/constants";
 
 interface Product {
     product: Weapon | Helmet | Armor | Boot | Ring | Artifact | Shield | Ingredient;
@@ -16,89 +16,28 @@ interface Product {
     isInCart?: boolean
 }
 
-interface Styles {
-    mainContainer: string;
-    imgContainer: string;
-    productImg: string;
-    infoContainer: string;
-    name: string;
-    requirementsContainer: string;
-    goldContainer: string;
-    levelContainer: string;
-    levelRequirement: string;
-    levelValue: string;
-}
-
 const ProductCard: React.FC<Product> = ({ product, onClick, isSelected, isInCart = false }) => {
-
-    const [componentStyles, setComponentStyles] = useState<Styles>({
-        mainContainer: '',
-        imgContainer: '',
-        productImg: '',
-        infoContainer: '',
-        name: '',
-        requirementsContainer: '',
-        goldContainer: '',
-        levelContainer: '',
-        levelRequirement: '',
-        levelValue: '',
-
-    })
-
-    useEffect(() => {
-
-        const shopStyles = {
-            mainContainer: `h-[41.5%] row-span-3 row-start-8 flex mt-[3%] bg-contain bg-no-repeat ${isSelected ? "bg-[url('/images/shop/product_card_selected.webp')]" : "bg-[url('/images/shop/product_card_unselected.webp')]"} transform transition-transform duration-300 hover:scale-105 cursor-pointer`,
-            imgContainer: "flex justify-center place-items-center place-items-center mt-[2.8%] w-[90%] h-[90%] ml-[2%] bg-contain bg-no-repeat bg-[url('/images/shop/product_image_container.webp')]",
-            productImg: "place-items-center self-center pb-[3%] w-[32%]",
-            infoContainer: "text-center content-center ml-[2%] w-[80%] space-y-[7%] inline-block",
-            name: "text-2xl text-white mr-[15%]",
-            requirementsContainer: "",
-            goldContainer: 'w-[50%] ml-[25%] align-center',
-            levelContainer: 'justify-center flex space-x-[15%] inline-row',
-            levelRequirement: 'text-xl w-[20%] text-white',
-            levelValue: 'text-4xl self-center'
-        }
-
-        const cartStyles = {
-            mainContainer: "flex h-[53%] w-[90%] row-span-3 row-start-8 place-items-center justify-center mt-[3%] ml-[5%] bg-contain bg-center bg-no-repeat bg-[url('/images/shop/product_card_unselected.webp')]",
-            imgContainer: "flex w-[35%] justify-center place-items-center h-[90%]  bg-contain bg-center bg-no-repeat bg-[url('/images/shop/product_image_container.webp')]",
-            productImg: 'w-[40%]',
-            infoContainer: 'w-[30%] h-[90%] place-items-center text-center justify-content-start space-y-[7%] inline-block',
-            name: "h-[30%]  ml-[5%] self-center w-[90%] text-2xl text-white",
-            requirementsContainer: "w-[100%] flex place-items-center justify-center align-center",
-            goldContainer: "w-[50%] flex  justify-content-start mr-[5%]",
-            levelContainer: "justify-end flex space-x-[50%] inline-row",
-            levelRequirement: "text-xl w-[2%] text-white",
-            levelValue: "text-4xl pr-[12%]"
-        }
-
-        const result = isInCart ? cartStyles : shopStyles
-
-        setComponentStyles(result)
-    }, [isInCart, isSelected])
-
     return (
         <>
             <div
-                className={componentStyles.mainContainer}
+                className={`${isInCart ? PRODUCT_CART.mainContainer : PRODUCT_SHOP.mainContainer} ${!isInCart && isSelected ? "bg-[url('/images/shop/product_card_selected.webp')]" : "bg-[url('/images/shop/product_card_unselected.webp')]"}`}
                 onClick={onClick}
             >
-                <div className={componentStyles.imgContainer}>
-                    <img className={componentStyles.productImg} src={`https://kaotika.vercel.app${product.image}`} alt="HeaderDivider" />
+                <div className={isInCart ? PRODUCT_CART.imgContainer : PRODUCT_SHOP.imgContainer}>
+                    <img className={isInCart ? PRODUCT_CART.productImg : PRODUCT_SHOP.productImg} src={`https://kaotika.vercel.app${product.image}`} alt="HeaderDivider" />
                 </div>
-                <div className={componentStyles.infoContainer}>
-                    <p className={componentStyles.name}>{product.name}</p>
-                    <div className={componentStyles.requirementsContainer}>
-                        <div className={componentStyles.goldContainer}>
+                <div className={isInCart ? PRODUCT_CART.infoContainer : PRODUCT_SHOP.infoContainer}>
+                    <p className={isInCart ? PRODUCT_CART.name : PRODUCT_SHOP.name}>{product.name}</p>
+                    <div className={isInCart ? PRODUCT_CART.requirementsContainer : PRODUCT_SHOP.requirementsContainer}>
+                        <div className={isInCart ? PRODUCT_CART.goldContainer : PRODUCT_SHOP.goldContainer}>
                             <GoldComponent amount={product.value} />
                         </div>
                         {
                             product.type !== 'ingredient'
                                 ? (
-                                    <div className={componentStyles.levelContainer}>
-                                        <p className={componentStyles.levelRequirement}>{`Req. Lvl.`}</p>
-                                        <p className={componentStyles.levelValue}>{product.min_lvl}</p>
+                                    <div className={isInCart ? PRODUCT_CART.levelContainer : PRODUCT_SHOP.levelContainer}>
+                                        <p className={isInCart ? PRODUCT_CART.levelRequirement : PRODUCT_SHOP.levelRequirement}>{`Req. Lvl.`}</p>
+                                        <p className={isInCart ? PRODUCT_CART.levelValue : PRODUCT_SHOP.levelValue}>{product.min_lvl}</p>
                                     </div>
                                 )
                                 : null
@@ -107,7 +46,6 @@ const ProductCard: React.FC<Product> = ({ product, onClick, isSelected, isInCart
                             isInCart ? (
                                 //ADD CART BUTTONS HERE
                                 <>
-                                
                                 </>
                             ) : null
                         }
