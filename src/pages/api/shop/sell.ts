c077d26653bf358223fe13a9075ed68c8ed4745f
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2024-12-10 20:21:26 +0100

    [ER6-536]UPDATE - the sell functionality

--- src/pages/api/shop/sell.ts
+++ src/pages/api/shop/sell.ts
@@ -9,6 +9,7 @@ import { Weapon } from '@/_common/interfaces/Weapon';
 import { Product } from '@/_common/types/Product';
 import { DBConnect, DBDisconnect } from '@/database/dbHandler';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { populatePlayer } from './player';
 const PlayerModel = require("../../../database/models/playerSchema");
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -20,19 +21,24 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
         await DBConnect();
         let mongoPlayer = await PlayerModel.findOne({ email: email });
-        console.log('Player gold: ', mongoPlayer.gold);
-        updatePlayerInventory(mongoPlayer, product);
-        updatePlayerGold(mongoPlayer, value);
-        console.log('Updated player gold: ', mongoPlayer.gold);
-        await mongoPlayer.save();
-        await DBDisconnect();
 
         if (mongoPlayer) {
-            return res.status(200).json(mongoPlayer);
-        }else {
+            console.log('Player gold: ', mongoPlayer.gold);
+            updatePlayerInventory(mongoPlayer, product);
+            updatePlayerGold(mongoPlayer, value);
+            console.log('Updated player gold: ', mongoPlayer.gold);
+            await mongoPlayer.save();
+            const returnPlayer = await populatePlayer()
+            console.log('PLAYER TO RETURN: ', returnPlayer);
+
+            await DBDisconnect();
+            return res.status(200).json(returnPlayer);
+        } else {
+            await DBDisconnect();
             return res.status(404).json({ error: 'Player returned is not correct' });
         }
     } catch (error) {
+        await DBDisconnect();
         return res.status(500).json({ error: 'Internal server error' });
     }
 }
@@ -45,37 +51,37 @@ const updatePlayerInventory = (player: Player, product: Product) => {
     switch(product.type) {
         case 'weapon':
             console.log('Player inventory weapons: ', player.inventory.weapons);
-            player.inventory.weapons = [...player.inventory.weapons.filter((item) => item._id !== product._id)] as Weapon[];
+            player.inventory.weapons = [...player.inventory.weapons.filter((item) => item._id.toString() !== product._id)];
             console.log('Updated player inventory weapons: ', player.inventory.weapons);
         break;
         case 'shield':
             console.log('Player inventory shields: ', player.inventory.shields);
-            player.inventory.shields = [...player.inventory.shields.filter((item) => item._id !== product._id)] as Shield[];
+            player.inventory.shields = [...player.inventory.shields.filter((item) => item._id.toString() !== product._id)];
             console.log('Updated player inventory shields: ', player.inventory.shields);
         break;
         case 'helmet':
             console.log('Player inventory helmets: ', player.inventory.helmets);
-            player.inventory.helmets = [...player.inventory.helmets.filter((item) => item._id !== product._id)] as Helmet[];
+            player.inventory.helmets = [...player.inventory.helmets.filter((item) => item._id.toString() !== product._id)];
             console.log('Updated player inventory helmets: ', player.inventory.helmets);
         break;
         case 'armor':
             console.log('Player inventory armors: ', player.inventory.armors);
-            player.inventory.armors = [...player.inventory.armors.filter((item) => item._id !== product._id)] as Armor[];
+            player.inventory.armors = [...player.inventory.armors.filter((item) => item._id.toString() !== product._id)];
             console.log('Updated player inventory armors: ', player.inventory.armors);
         break;
         case 'boot':
             console.log('Player inventory boots: ', player.inventory.boots);
-            player.inventory.boots = [...player.inventory.boots.filter((item) => item._id !== product._id)] as Boot[];
+            player.inventory.boots = [...player.inventory.boots.filter((item) => item._id.toString() !== product._id)];
             console.log('Updated player inventory boots: ', player.inventory.boots);
         break;
         case 'ring':
             console.log('Player inventory rings: ', player.inventory.rings);
-            player.inventory.rings = [...player.inventory.rings.filter((item) => item._id !== product._id)] as Ring[];
+            player.inventory.rings = [...player.inventory.rings.filter((item) => item._id.toString() !== product._id)];
             console.log('Updated player inventory rings: ', player.inventory.rings);
         break;
         case 'artifact':
             console.log('Player inventory artifacts: ', player.inventory.artifacts);
-            player.inventory.artifacts = [...player.inventory.artifacts.filter((item) => item._id !== product._id)] as Artifact[];
+            player.inventory.artifacts = [...player.inventory.artifacts.filter((item) => item._id.toString() !== product._id)];
             console.log('Updated player inventory artifacts: ', player.inventory.artifacts);
         break;
     };
