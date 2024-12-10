cf202a7f3e18e913b6e20f42e20c010ed22091d1
Unai <reikrb@gmail.com>
2024-12-10 13:37:35 +0100

    [ER6-352] UPDATE - Fetch player to populate ids

--- src/pages/api/shop/player.ts
+++ src/pages/api/shop/player.ts
@@ -12,17 +12,46 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     try {
         await DBConnect()
-        const response = await Player.findOne({ email: 'unai.roca@ikasle.aeg.eus' });
-        
+        const response = await populatePlayer()
         await DBDisconnect()
         if (response) {
             return res.status(200).json(response);
-        }else {
+        } else {
             return res.status(404).json(response);
         }
-         
+
     } catch (error) {
         return res.status(500).json({ error: 'Internal server error' });
     }
-    
+
 }
+
+const populatePlayer = async () => {
+
+    
+    const playerPopulated: any = await Player.findOne({ email: 'unai.roca@ikasle.aeg.eus' }).populate('profile').exec();
+
+    // Poblamos el equipo
+    await playerPopulated.equipment.populate('armor');
+    await playerPopulated.equipment.populate('weapon');
+    await playerPopulated.equipment.populate('artifact');
+    await playerPopulated.equipment.populate('ring');
+    await playerPopulated.equipment.populate('helmet');
+    await playerPopulated.equipment.populate('shield');
+    await playerPopulated.equipment.populate('boot');
+
+
+    // Poblamos el inventario
+    await playerPopulated.inventory.populate('helmets');
+    await playerPopulated.inventory.populate('shields');
+    await playerPopulated.inventory.populate('weapons');
+    await playerPopulated.inventory.populate('boots');
+    await playerPopulated.inventory.populate('rings');
+    await playerPopulated.inventory.populate('armors');
+    await playerPopulated.inventory.populate('artifacts');
+    await playerPopulated.inventory.populate('ingredients');
+
+    console.log('playerPopulated', playerPopulated);
+    return playerPopulated;
+}
+
