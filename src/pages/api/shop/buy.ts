0a469ac7d17a25508360956919380646fad9443c
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2024-12-10 18:30:09 +0100

    [ER6-535]UPDATE - the buy functionality to only save the id of the product

--- src/pages/api/shop/buy.ts
+++ src/pages/api/shop/buy.ts
@@ -5,74 +5,82 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const PlayerModel = require("../../../database/models/playerSchema");
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const email = req.body.email;
-    const productsData = req.body.products;
-    const value = calculatePurchaseValue(productsData);
-    console.log('Products value: ', value);
-
     try {
         await DBConnect();
+        const email = req.body.email;
+        const productsData = req.body.products;
         let mongoPlayer = await PlayerModel.findOne({ email: email });
-        const productInInventory = isProductInTheInventory(mongoPlayer, productsData);
-        const productEquiped = isProductEquiped(mongoPlayer, productsData);
-        const goldSufficient = isGoldSufficient(mongoPlayer, value);
-        console.log('Products is not in the inventory: ', productInInventory);
-        console.log('Products is not equiped: ', productInInventory);
-        console.log('Gold is sufficient: ', goldSufficient);
+        
+        if (mongoPlayer){
+            const value = calculatePurchaseValue(productsData);
+            console.log('Products value: ', value);
+            const productInInventory = isProductInTheInventory(mongoPlayer, productsData);
+            const productEquiped = isProductEquiped(mongoPlayer, productsData);
+            const goldSufficient = isGoldSufficient(mongoPlayer, value);
+            console.log('Products is not in the inventory: ', productInInventory);
+            console.log('Products is not equiped: ', productInInventory);
+            console.log('Gold is sufficient: ', goldSufficient);
 
-        if (productInInventory && productEquiped) {
-            if (goldSufficient) {
-                console.log('Player inventory: ', mongoPlayer.inventory);
-                console.log('Player gold: ', mongoPlayer.gold);
-                updatePlayerInventory(mongoPlayer, productsData);
-                updatePlayerGold(mongoPlayer, value);
-                console.log('Updated player inventory: ', mongoPlayer.inventory);
-                console.log('Updated player gold: ', mongoPlayer.gold);
-                await mongoPlayer.save();
+            if (productInInventory && productEquiped) {
+                if (goldSufficient) {
+                    console.log('Player inventory: ', mongoPlayer.inventory);
+                    console.log('Player gold: ', mongoPlayer.gold);
+                    updatePlayerInventory(mongoPlayer, productsData);
+                    updatePlayerGold(mongoPlayer, value);
+                    console.log('Updated player inventory: ', mongoPlayer.inventory);
+                    console.log('Updated player gold: ', mongoPlayer.gold);
+                    await mongoPlayer.save();
+                    await DBDisconnect();
+                    return res.status(200).json(mongoPlayer);
+                } else {
+                    await DBDisconnect();
+                    console.log(`Player gold insufficient: players gold is ${mongoPlayer.gold} and the product value is ${value}`);
+                    return res.status(400).json({ player: mongoPlayer, error: `Player gold insufficient: players gold is ${mongoPlayer.gold} and the product value is ${value}` });
+                }
             } else {
-                console.log(`Player gold insufficient: players gold is ${mongoPlayer.gold} and the product value is ${value}`);
-                return res.status(400).json({ player: mongoPlayer, error: `Player gold insufficient: players gold is ${mongoPlayer.gold} and the product value is ${value}` });
+                await DBDisconnect();
+                console.log('Player already has the product');
+                return res.status(409).json({ player: mongoPlayer, error: 'Player already has the product' });
             }
         } else {
-            console.log('Player already has the product');
-            return res.status(409).json({ player: mongoPlayer, error: 'Player already has the product' });
-        }
-        
-        await DBDisconnect();
-
-        if (mongoPlayer) {
-            return res.status(200).json(mongoPlayer);
-        }else {
+            await DBDisconnect();
             return res.status(404).json({ error: 'Player returned is not correct' });
         }
     } catch (error) {
+        await DBDisconnect();
         return res.status(500).json({ error: 'Internal server error' });
     }
 }
 
 const updatePlayerInventory = (player: Player, products: string) => {
     products.map((product) => {
-        switch(product.type) {
+        console.log(product.product._id);
+        
+        switch(product.product.type) {
             case 'weapon':  
-                player.inventory.weapons = [...player.inventory.weapons, product._id];
+                player.inventory.weapons = [...player.inventory.weapons, product.product._id];
+                console.log(player.inventory);
             break;
             case 'shield':
-                player.inventory.shields = [...player.inventory.shields, product._id];
+                player.inventory.shields = [...player.inventory.shields, product.product._id];
             break;
             case 'helmet':
-                player.inventory.helmets = [...player.inventory.helmets, product._id];
+                player.inventory.helmets = [...player.inventory.helmets, product.product._id];
             break;
             case 'armor':
-                player.inventory.armors = [...player.inventory.armors, product._id];
+                player.inventory.armors = [...player.inventory.armors, product.product._id];
             break;
             case 'boot':
-                player.inventory.boots = [...player.inventory.boots, product._id];
+                player.inventory.boots = [...player.inventory.boots, product.product._id];
             break;
             case 'ring':
-                player.inventory.rings = [...player.inventory.rings, product._id];
+                player.inventory.rings = [...player.inventory.rings, product.product._id];
             break;
             case 'artifact':
-                player.inventory.artifacts = [...player.inventory.artifacts, product._id];
+                player.inventory.artifacts = [...player.inventory.artifacts, product.product._id];
+            break;
+            case 'ingredient':
+                player.inventory.ingredients = [...player.inventory.ingredients, product.product._id];
             break;
         };
     });
