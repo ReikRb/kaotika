97b9ca6249ae56620008ba040a85f3792d59d693
Unai <reikrb@gmail.com>
2024-12-10 10:31:45 +0100

    [ER6-351] UPDATE - Player Schema

--- src/database/models/playerSchema.ts
+++ src/database/models/playerSchema.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const playerSchema = new mongoose.Schema({
     attributes: {
@@ -10,127 +10,29 @@ const playerSchema = new mongoose.Schema({
       strength: { type: 'Number' }
     },
     equipment: {
-      weapon: {
-        modifiers: Object,
-        _id: Object,
-        name: Object,
-        description: Object,
-        type: Object,
-        image: Object,
-        value: Object,
-        damage: Object,
-        base_percentage: Object,
-        min_lvl: Object
-      },
-      armor: {
-        modifiers: Object,
-        _id: Object,
-        name: Object,
-        description: Object,
-        type: Object,
-        image: Object,
-        value: Object,
-        defense: Object,
-        min_lvl: Object
-      },
-      artifact: {
-        modifiers: Object,
-        _id: Object,
-        name: Object,
-        description: Object,
-        type: Object,
-        image: Object,
-        value: Object,
-        min_lvl: Object
-      },
-      antidote_potion: {
-        _id: Object,
-        name: Object,
-        description: Object,
-        type: Object,
-        image: Object,
-        value: Object,
-        recovery_effect: Object,
-        min_lvl: Object
-      },
-      healing_potion: {
-        modifiers: Object,
-        _id: Object,
-        name: Object,
-        description: Object,
-        type: Object,
-        image: Object,
-        value: Object,
-        min_lvl: Object
-      },
-      enhancer_potion: {
-        modifiers: Object,
-        _id: Object,
-        name: Object,
-        description: Object,
-        type: Object,
-        image: Object,
-        value: Object,
-        duration: Object,
-        min_lvl: Object
-      },
-      helmet: {
-        modifiers: Object,
-        _id: Object,
-        name: Object,
-        description: Object,
-        type: Object,
-        image: Object,
-        value: Object,
-        defense: Object,
-        min_lvl: Object
-      },
-      shield: {
-        modifiers: Object,
-        _id: Object,
-        name: Object,
-        description: Object,
-        type: Object,
-        image: Object,
-        value: Object,
-        defense: Object,
-        min_lvl: Object,
-        isUnique: Object,
-        isActive: Object
-      },
-      boot: {
-        modifiers: Object,
-        _id: Object,
-        name: Object,
-        description: Object,
-        type: Object,
-        image: Object,
-        value: Object,
-        defense: Object,
-        min_lvl: Object
-      },
-      ring: {
-        modifiers: Object,
-        _id: Object,
-        name: Object,
-        description: Object,
-        type: Object,
-        image: Object,
-        value: Object,
-        min_lvl: Object
-      }
+      helmet: { type: Schema.Types.ObjectId, ref: "Helmet", default: "66d99aac7518eb4990035363" },
+      weapon: { type: Schema.Types.ObjectId, ref: "Weapon" },
+      armor: { type: Schema.Types.ObjectId, ref: "Armor" },
+      shield: { type: Schema.Types.ObjectId, ref: "Shield", default: "66f27c81c114335cadf45d70" },
+      artifact: { type: Schema.Types.ObjectId, ref: "Artifact" },
+      boot: { type: Schema.Types.ObjectId, ref: "Boot", default: "66d99a807518eb499003535f" },
+      ring: { type: Schema.Types.ObjectId, ref: "Ring", default: "66a6d6c8dfbffe7e6503970f" },
+      antidote_potion: { type: Schema.Types.ObjectId, ref: "PotionAntidote" },
+      healing_potion: { type: Schema.Types.ObjectId, ref: "PotionHealing" },
+      enhancer_potion: { type: Schema.Types.ObjectId, ref: "PotionEnhancer" }
     },
     inventory: {
-      helmets: Array,
-      weapons: Array,
-      armors: Array,
-      shields: Array,
-      artifacts: Array,
-      boots: Array,
-      rings: Array,
-      antidote_potions: Array,
-      healing_potions: Array,
-      enhancer_potions: Array
+      helmets: [{ type: Schema.Types.ObjectId, ref: "Helmet" }],
+      weapons: [{ type: Schema.Types.ObjectId, ref: "Weapon" }],
+      armors: [{ type: Schema.Types.ObjectId, ref: "Armor" }],
+      shields: [{ type: Schema.Types.ObjectId, ref: "Shield" }],
+      artifacts: [{ type: Schema.Types.ObjectId, ref: "Artifact" }],
+      boots: [{ type: Schema.Types.ObjectId, ref: "Boot" }],
+      rings: [{ type: Schema.Types.ObjectId, ref: "Ring" }],
+      antidote_potions: [{ type: Schema.Types.ObjectId, ref: "PotionAntidote" }],
+      healing_potions: [{ type: Schema.Types.ObjectId, ref: "PotionHealing" }],
+      enhancer_potions: [{ type: Schema.Types.ObjectId, ref: "PotionEnhancer" }],
+      ingredients: [{ type: Schema.Types.ObjectId, ref: "Ingredient" }]
     },
     _id: { type: 'ObjectId' },
     name: { type: 'String' },
