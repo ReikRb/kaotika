7fd8c43ec91bafc684166520787746fe20d0efa7
victor06Duran <victor.duran@ikasle.aeg.eus>
2024-12-11 09:49:25 +0100

    [ER6-546]ADD - If selling and ingredient, limit qty component value to product qty

--- src/components/shop/MidContainer.tsx
+++ src/components/shop/MidContainer.tsx
@@ -17,156 +17,161 @@ import ConfirmationComponent from "./ConfirmationComponent";
 import { Product } from "@/_common/types/Product";
 
 interface Props {
-    product: Product;
-    onBuy: (products: {product: Product, quantity: number}[], isInCart: boolean) => void;
-    onSell: (product: {product: Product, quantity: number}) => void;
-    onAddToCart: (product: Product, quantity: number) => void;
-    player: Player;
-    quantity: number;
-    handleQuantityChange: (product: Product, quantity: number) => void;
-    displayBuyButtons: boolean;
+  product: Product;
+  onBuy: (products: { product: Product, quantity: number }[], isInCart: boolean) => void;
+  onSell: (product: { product: Product, quantity: number }) => void;
+  onAddToCart: (product: Product, quantity: number) => void;
+  player: Player;
+  quantity: number;
+  handleQuantityChange: (product: Product, quantity: number) => void;
+  displayBuyButtons: boolean;
+  isSelling: boolean;
 };
 
 const hasDefense = (product: Product): product is (Helmet | Armor | Boot | Shield) => {
-    return "defense" in product;
+  return "defense" in product;
 };
 
 const isWeapon = (product: Product): product is Weapon => {
-    return "base_percentage" in product && "die_faces" in product;
+  return "base_percentage" in product && "die_faces" in product;
 };
 
 const isMagical = (product: Product): product is Ingredient => {
-    return "effects" in product;
+  return "effects" in product;
+};
+const isIngredient = (product: Product): product is Ingredient => {
+  return "qty" in product;
 };
 
-const MidContainer: React.FC<Props> = ({ product, onBuy, onSell, onAddToCart, player, quantity, displayBuyButtons }) => {
-    const [isModalOpen, setModalOpen] = useState(false);
-    const [modalContent, setModalContent] = useState<{ name: string; value: number } | null>(null);
-    const [localQuantity, setLocalQuantity] = useState<number>(1);
-    const handleOpenModal = () => {
-        if (product) {
-            setModalContent({ name: product.name, value: product.value * localQuantity });
-            setModalOpen(true);
-        }
-    };
-
-    const handleBuy = () => {
-        onBuy([{product: product, quantity: localQuantity}], false);
-        setModalOpen(false);
-        setModalContent(null);
-    };
-
-    const handleSell = () => {
-        onSell({product: product, quantity: localQuantity});
-        setModalOpen(false);
-        setModalContent(null);
-    };
-
-    const handleCloseModal = () => {
-        setModalOpen(false);
-        setModalContent(null);
-    };
-
-    const handleLocalQuantityChange = (newQuantity: number) => {
-        setLocalQuantity(newQuantity);
-    };
-
-
-
-    useEffect(() => {
-        if (product) {
-            setLocalQuantity(quantity);
-        }
-    }, [product, quantity]);
-
-    const canAfford = () => {
-        const value = calculatePurchaseValue([product], localQuantity);
-
-        return (
-            isProductInTheInventory(player, [product]) &&
-            isProductEquiped(player, [product]) &&
-            isGoldSufficient(player, value)
-        );
-    };
-
-    if (!product) {
-        return <p className="w-4/12 h-full flex justify-center items-center 2xl:text-3xl lg:text-xl sm:text-base">Select a product to view details.</p>;
+const MidContainer: React.FC<Props> = ({ product, onBuy, onSell, onAddToCart, player, quantity, displayBuyButtons}) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState<{ name: string; value: number } | null>(null);
+  const [localQuantity, setLocalQuantity] = useState<number>(1);
+  const handleOpenModal = () => {
+    if (product) {
+      setModalContent({ name: product.name, value: product.value * localQuantity });
+      setModalOpen(true);
     }
+  };
+
+  const handleBuy = () => {
+    onBuy([{ product: product, quantity: localQuantity }], false);
+    setModalOpen(false);
+    setModalContent(null);
+  };
+
+  const handleSell = () => {
+    onSell({ product: product, quantity: localQuantity });
+    setModalOpen(false);
+    setModalContent(null);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+    setModalContent(null);
+  };
+
+  const handleLocalQuantityChange = (newQuantity: number) => {
+    setLocalQuantity(newQuantity);
+  };
+
+
+
+  useEffect(() => {
+    if (product) {
+      setLocalQuantity(quantity);
+    }
+  }, [product, quantity]);
+
+  const canAfford = () => {
+    const value = calculatePurchaseValue([product], localQuantity);
 
     return (
-        <>
-            <div className="w-4/12 grid grid-rows-12">
-                {isModalOpen && modalContent && (
-                    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 flex items-center justify-center z-50">
-                        <ConfirmationComponent 
-                        displayBuyButtons={displayBuyButtons} 
-                        quantity={localQuantity} 
-                        modalContent={modalContent} 
-                        handleBuy={handleBuy} 
-                        handleSell={handleSell} 
-                        handleCloseModal={handleCloseModal}
-                        />
-                    </div>
-                )}
-                {isMagical(product) ? (
-                    <div className="w-full row-span-1 row-start-0 flex justify-center place-content-center pt-[2%]">
-                        <RequirementsSection gold={product.value * localQuantity} />
-                    </div>
-                ) : (
-                    <div className="w-full row-span-1 row-start-0 flex justify-center place-content-center pt-[2%]">
-                        <RequirementsSection gold={displayBuyButtons ? product.value * localQuantity : Math.floor(product.value * localQuantity/3)} level={product.min_lvl}/>
-                    </div>
-                )}
-                {isMagical(product) ? (
-                    <div />
-                ) : (
-                    <div className="w-full row-span-3 row-start-2 flex justify-center place-content-center pt-[2%]">
-                        {hasDefense(product) ? (
-                            <ProductDefenseDisplay defense={product.defense} />
-                        ) : isWeapon(product) ? (
-                            <ProductWeaponDisplay basePercentage={product.base_percentage} dieFaces={product.die_faces} dieModifier={product.die_modifier} dieNum={product.die_num} />
-                        ) : (
-                            <div />
-                        )}
-                    </div>
-                )}
-                <div className={isMagical(product) ? "w-full row-span-5 row-start-3 flex justify-center place-content-center p-[2%]" : "w-full row-span-5 row-start-5 flex justify-center place-content-center p-[2%]"}>
-                    <ProductImage imageSrc={product.image} altText={product.name} />
-                </div>
-                {isMagical(product) ? (
-                    <div className="w-full row-span-2 row-start-8 flex items-center justify-center p-[2%]">
-                        <IncrementDecrement
-                            initialValue={localQuantity}
-                            onValueChange={(_, newQuantity) => handleLocalQuantityChange(newQuantity)}
-                            isInCart={false}
-                            product={product}
-                        />
-                    </div>
-                ) : (
-                    <div />
-                )}
-                <div className="w-full row-span-3 row-start-10 flex justify-around items-center p-[2%]">
-                    {displayBuyButtons ? (
-                        <>
-                            <ShopButton label="BUY" canAfford={canAfford} onClick={canAfford() ? handleOpenModal : () => { }} />
-                            <ShopButton
-                                label="ADD TO CART"
-                                canAfford={canAfford}
-                                onClick={canAfford() ? () => {
-                                    if (product) {
-                                        onAddToCart(product, localQuantity);
-                                        setLocalQuantity(1);
-                                    }
-                                } : () => { }}
-                            />
-                        </>
-                    ) : (
-                        <ShopButton label="SELL" canAfford={() => { return true }} onClick={handleOpenModal} />
-                    )}
-                </div>
-            </div>
-        </>
+      isProductInTheInventory(player, [product]) &&
+      isProductEquiped(player, [product]) &&
+      isGoldSufficient(player, value)
     );
+  };
+
+  if (!product) {
+    return <p className="w-4/12 h-full flex justify-center items-center 2xl:text-3xl lg:text-xl sm:text-base">Select a product to view details.</p>;
+  }
+
+  return (
+    <>
+      <div className="w-4/12 grid grid-rows-12">
+        {isModalOpen && modalContent && (
+          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 flex items-center justify-center z-50">
+            <ConfirmationComponent
+              displayBuyButtons={displayBuyButtons}
+              quantity={localQuantity}
+              modalContent={modalContent}
+              handleBuy={handleBuy}
+              handleSell={handleSell}
+              handleCloseModal={handleCloseModal}
+            />
+          </div>
+        )}
+        {isMagical(product) ? (
+          <div className="w-full row-span-1 row-start-0 flex justify-center place-content-center pt-[2%]">
+            <RequirementsSection gold={product.value * localQuantity} />
+          </div>
+        ) : (
+          <div className="w-full row-span-1 row-start-0 flex justify-center place-content-center pt-[2%]">
+            <RequirementsSection gold={displayBuyButtons ? product.value * localQuantity : Math.floor(product.value * localQuantity / 3)} level={product.min_lvl} />
+          </div>
+        )}
+        {isMagical(product) ? (
+          <div />
+        ) : (
+          <div className="w-full row-span-3 row-start-2 flex justify-center place-content-center pt-[2%]">
+            {hasDefense(product) ? (
+              <ProductDefenseDisplay defense={product.defense} />
+            ) : isWeapon(product) ? (
+              <ProductWeaponDisplay basePercentage={product.base_percentage} dieFaces={product.die_faces} dieModifier={product.die_modifier} dieNum={product.die_num} />
+            ) : (
+              <div />
+            )}
+          </div>
+        )}
+        <div className={isMagical(product) ? "w-full row-span-5 row-start-3 flex justify-center place-content-center p-[2%]" : "w-full row-span-5 row-start-5 flex justify-center place-content-center p-[2%]"}>
+          <ProductImage imageSrc={product.image} altText={product.name} />
+        </div>
+        {isMagical(product) ? (
+          <div className="w-full row-span-2 row-start-8 flex items-center justify-center p-[2%]">
+            <IncrementDecrement
+              initialValue={localQuantity}
+              onValueChange={(_, newQuantity) => handleLocalQuantityChange(newQuantity)}
+              isInCart={false}
+              product={product}
+              displayButtons={!displayBuyButtons}
+            />
+          </div>
+        ) : (
+          <div />
+        )}
+        <div className="w-full row-span-3 row-start-10 flex justify-around items-center p-[2%]">
+          {displayBuyButtons ? (
+            <>
+              <ShopButton label="BUY" canAfford={canAfford} onClick={canAfford() ? handleOpenModal : () => { }} />
+              <ShopButton
+                label="ADD TO CART"
+                canAfford={canAfford}
+                onClick={canAfford() ? () => {
+                  if (product) {
+                    onAddToCart(product, localQuantity);
+                    setLocalQuantity(1);
+                  }
+                } : () => { }}
+              />
+            </>
+          ) : (
+            <ShopButton label="SELL" canAfford={() => { return true }} onClick={handleOpenModal} />
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MidContainer;
--- src/components/shop/UpdateQtyButton.tsx
+++ src/components/shop/UpdateQtyButton.tsx
@@ -11,73 +11,91 @@ import { Weapon } from "@/_common/interfaces/Weapon";
 type Product = Weapon | Helmet | Armor | Boot | Ring | Artifact | Shield | Ingredient;
 
 interface IncrementDecrementProps {
-    initialValue?: number;
-    onValueChange: (product: Product, quantity: number) => void;
-    isInCart: boolean;
-    product: Weapon | Helmet | Armor | Boot | Ring | Artifact | Shield | Ingredient;
+  initialValue?: number;
+  onValueChange: (product: Product, quantity: number) => void;
+  isInCart: boolean;
+  product: Weapon | Helmet | Armor | Boot | Ring | Artifact | Shield | Ingredient;
+  displayButtons: boolean;
 }
 
-const IncrementDecrement: React.FC<IncrementDecrementProps> = ({ initialValue = 1, onValueChange, isInCart = false, product }) => {
-    const [count, setCount] = useState(initialValue);
+const IncrementDecrement: React.FC<IncrementDecrementProps> = ({ initialValue = 1, onValueChange, isInCart = false, product, displayButtons }) => {
+  const [count, setCount] = useState(initialValue);
 
-    useEffect(() => {
-        setCount(initialValue);
-    }, [initialValue]);
+  const isIngredient = (product: Product): product is Ingredient => {
+    return "qty" in product;
+  };
 
-    const handleIncrement = () => {
-        let newValue = count + 1;
-        if (newValue > 99) {
-            newValue = 99;
-        }
-        setCount(newValue);
-        onValueChange(product, newValue);
-    };
+  useEffect(() => {
+    setCount(initialValue);
+  }, [initialValue]);
 
-    const handleDecrement = () => {
-        let newValue = count - 1;
-        if (!isInCart && newValue < 1) {
-            newValue = 1;
-        }
-        if (newValue >= 0) {
-            setCount(newValue);
-            onValueChange(product, newValue);
-        }
-    };
+  const handleIncrement = () => {
+    let newValue = count + 1;
+  
+    if (displayButtons && isIngredient(product)) {
+      const maxValue = displayButtons ? product.qty : 99;
+      newValue = Math.min(newValue, maxValue);
+    }
+  
+    if (newValue <= 99) {
+      setCount(newValue);
+      onValueChange(product, newValue);
+    }
+  };
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        let value = Math.max(0, parseInt(e.target.value) || 0);
-        if (!isInCart && value < 1) {
-            value = 1;
-        }
-        if (value > 99) {
-            value = 99;
-        }
-        setCount(value);
-        onValueChange(product, value);
-    };
+  const handleDecrement = () => {
+    let newValue = count - 1;
+    if (!isInCart && newValue < 1) {
+      newValue = 1;
+    }
+    if (newValue >= 0) {
+      setCount(newValue);
+      onValueChange(product, newValue);
+    }
+  };
 
-    return (
-        <div className="w-full flex items-center justify-center">
-            <button
-                className="w-[20%] cursor-pointer bg-[url('/images/shop/UpdateQtyBox.png')] bg-contain bg-center bg-no-repeat text-white 2xl:text-4xl lg:text-3xl sm:text-2xl text-xl"
-                onClick={count < 99 ? handleIncrement : () => { }}>
-                <p className="pb-[7%]">+</p>
-            </button>
-            <input
-                type="number"
-                value={count}
-                min={isInCart ? 0 : 1}
-                max={99}
-                className="w-1/9 cursor-pointer text-center pb-[1%] text-white 2xl:text-4xl lg:text-3xl sm:text-1xl text-xl border border-x-sepia rounded bg-black
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let value = Math.max(0, parseInt(e.target.value) || 0);
+  
+    if (displayButtons && isIngredient(product)) {
+      const maxValue = displayButtons ? product.qty : 99;
+      value = Math.min(value, maxValue);
+    }
+  
+    if (!isInCart && value < 1) {
+      value = 1;
+    }
+  
+    if (value > 99) {
+      value = 99;
+    }
+  
+    setCount(value);
+    onValueChange(product, value);
+  };
+
+  return (
+    <div className="w-full flex items-center justify-center">
+      <button
+        className="w-[20%] cursor-pointer bg-[url('/images/shop/UpdateQtyBox.png')] bg-contain bg-center bg-no-repeat text-white 2xl:text-4xl lg:text-3xl sm:text-2xl text-xl"
+        onClick={count < 99 ? handleIncrement : () => { }}>
+        <p className="pb-[7%]">+</p>
+      </button>
+      <input
+        type="number"
+        value={count}
+        min={isInCart ? 0 : 1}
+        max={displayButtons && isIngredient(product) ? product.qty : 99}
+        className="w-1/9 cursor-pointer text-center pb-[1%] text-white 2xl:text-4xl lg:text-3xl sm:text-1xl text-xl border border-x-sepia rounded bg-black
                 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-                onChange={handleInputChange}/>
-            <button
-                className="w-[20%] cursor-pointer bg-[url('/images/shop/UpdateQtyBox.png')] bg-contain bg-center bg-no-repeat text-white 2xl:text-4xl lg:text-3xl sm:text-2xl text-xl"
-                onClick={handleDecrement}>
-                <p className="pb-[7%]">-</p>
-            </button>
-        </div>
-    );
+        onChange={handleInputChange} />
+      <button
+        className="w-[20%] cursor-pointer bg-[url('/images/shop/UpdateQtyBox.png')] bg-contain bg-center bg-no-repeat text-white 2xl:text-4xl lg:text-3xl sm:text-2xl text-xl"
+        onClick={handleDecrement}>
+        <p className="pb-[7%]">-</p>
+      </button>
+    </div>
+  );
 };
 
 export default IncrementDecrement;
