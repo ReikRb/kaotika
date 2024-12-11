e0b757767f24086c2889ad7b6408685b40cf7ce6
Unai <reikrb@gmail.com>
2024-12-11 12:52:40 +0100

    [ER6-540] ADD - Merchant borders

--- src/components/shop/MerchantComponent.tsx
+++ src/components/shop/MerchantComponent.tsx
@@ -26,10 +26,12 @@ const MerchantComponent: React.FC<Props> = ({ message }) => {
 
     return (
         <>
-            <div className="w-4/12 h-full bg-[url('/images/shop/merchant.png')] bg-cover bg-center"/>
-            <div className="w-8/12 h-full grid p-[2%] overflow-y-auto">
+            <div className="w-4/12 h-full bg-[url('/images/shop/merchant.png')] bg-cover bg-center border-2 border-darkSepia "/>
+            <div className="w-8/12 h-full grid p-[2%] overflow-y-auto border-y-1 border-r-1 bg-gray-950 bg-opacity-70 border-darkSepia ">
                 <p className="2xl:text-4xl lg:text-2xl sm:text-xl pb-[2%]">Merchant</p>
-                <p className="2xl:text-3xl lg:text-xl sm:text-lg text-white">{text}</p>
+                <div className="overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-950 [&::-webkit-scrollbar-thumb]:bg-gray-800">
+                    <p className="2xl:text-4xl lg:text-xl sm:text-lg text-white">{text}</p>
+                </div>
             </div>
         </>
     );
