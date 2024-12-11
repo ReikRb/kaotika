a21c9ff6507c8fa9a591c1f42bad48df0a66db4a
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2024-12-11 13:22:19 +0100

    [ER6-544]UPDATE - the merchant text size

--- src/components/shop/MerchantComponent.tsx
+++ src/components/shop/MerchantComponent.tsx
@@ -30,7 +30,7 @@ const MerchantComponent: React.FC<Props> = ({ message }) => {
             <div className="w-8/12 h-full grid p-[2%] overflow-y-auto border-y-1 border-r-1 bg-gray-950 bg-opacity-70 border-darkSepia ">
                 <p className="2xl:text-4xl lg:text-2xl sm:text-xl pb-[2%]">Merchant</p>
                 <div className="overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-950 [&::-webkit-scrollbar-thumb]:bg-gray-800">
-                    <p className="2xl:text-4xl lg:text-xl sm:text-lg text-white">{text}</p>
+                    <p className="2xl:text-3xl lg:text-xl sm:text-lg text-white">{text}</p>
                 </div>
             </div>
         </>
