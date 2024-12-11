d3fa34b0d8dd611eedc109bdc8754885370b9619
Unai <reikrb@gmail.com>
2024-12-11 20:15:16 +0100

    [ER6-548] UPDATE - fonts in mid container components

--- src/components/shop/DefensiveModifierDisplay.tsx
+++ src/components/shop/DefensiveModifierDisplay.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const ProductDefense: React.FC<{ defense: number }> = ({ defense }) => (
     <>
-        <span className="text-gray-200 2xl:text-2xl lg:text-lg sm:text-base">Defense</span>
-        <span className="text-amber-200 2xl:text-2xl lg:text-lg sm:text-base">{defense}</span>
+        <span className="text-gray-200 2xl:text-3xl lg:text-lg sm:text-base">Defense</span>
+        <span className="text-amber-200 2xl:text-4xl lg:text-lg sm:text-base">{defense}</span>
     </>
 );
 
 const ProductDefenseDisplay: React.FC<{ defense?: number }> = ({ defense }) => (
     <>
         {defense ? (
-            <div className="w-full grid grid-flow-col justify-around place-content-center bg-[url('/images/shop/ProductBonusContainer.png')] bg-contain bg-center bg-no-repeat">
+            <div className=" w-full grid grid-flow-col justify-around place-content-center bg-[url('/images/shop/ProductBonusContainer.png')] bg-contain bg-center bg-no-repeat">
                 <ProductDefense defense={defense}/>
             </div>
         ) : (
--- src/components/shop/shopButton.tsx
+++ src/components/shop/shopButton.tsx
@@ -12,7 +12,7 @@ const ShopButton: React.FC<Props> = ({ label, canAfford, onClick }) => {
             "w-5/12 h-full flex items-center justify-center cursor-pointer hover:opacity-80 focus:opacity-80 bg-[url('/images/shop/store_button.webp')] bg-contain bg-center bg-no-repeat"
             : "w-5/12 h-full flex items-center justify-center cursor-pointer hover:opacity-80 focus:opacity-80 bg-[url('/images/shop/disabled_store_button.webp')] bg-contain bg-center bg-no-repeat"}
             onClick={onClick}>
-            <span className="inset-0 text-white 2xl:text-base lg:text-xs sm:text-xs">
+            <span className="inset-0 text-white 2xl:text-xl lg:text-xs sm:text-xs">
                 {label}
             </span>
         </div>
