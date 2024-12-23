870edccad848fc863264b4234e1bd27fb8f6706f
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2024-12-23 13:40:19 +0100

    [ER6-557]UPDATE - the players mocks

--- src/__tests__/api/buy.test.ts
+++ src/__tests__/api/buy.test.ts
@@ -1,49 +1,68 @@
 import { UnPopulatedPlayer } from "@/_common/interfaces/UnPopulatedPlayer";
 import { Cart } from "@/_common/types/Product";
-import { MOCK_INGREDIENTS_COLLECTION, MOCK_SHIELDS_COLLECTION } from "../mocks";
+import { MOCK_INGREDIENTS_COLLECTION, MOCK_SHIELDS_COLLECTION, MOCK_UNPOPULATED_PLAYER } from "../mocks";
 import { updatePlayerGold, updatePlayerInventory } from "@/helpers/updatePlayer";
 
-const mockPlayer: UnPopulatedPlayer = {
-  inventory: {
-    helmets: [],
-    weapons: [],
-    armors: [],
-    shields: [],
-    artifacts: [],
-    boots: [],
-    rings: [],
-    antidote_potions: [],
-    healing_potions: [],
-    enhancer_potions: [],
-    ingredients: [],
-  },
-  gold: 5750,
-};
+const mockPlayer: UnPopulatedPlayer = MOCK_UNPOPULATED_PLAYER;
 
 const mockUpdatedPlayer: UnPopulatedPlayer = {
-  inventory: {
-    helmets: [],
-    weapons: [],
-    armors: [],
-    shields: ['66f27c81c114335cadf45d70'],
-    artifacts: [],
-    boots: [],
-    rings: [],
-    antidote_potions: [],
-    healing_potions: [],
-    enhancer_potions: [],
-    ingredients: [
-      '6702b39d76863c206a48cccb',
-      '6702b39d76863c206a48cccb',
-      '6702b39d76863c206a48cccb',
-      '6702b39d76863c206a48cccb',
-      '6702b39d76863c206a48cccb',
-      '6702b39d76863c206a48cccb',
-      '6702b39d76863c206a48cccb',
-      '6702b39d76863c206a48cccb',
-    ],
-  },
-  gold: 5250,
+    _id: '',
+    avatar: '',
+    email: '',
+    experience: 0,
+    is_active: false,
+    created_date: '',
+    profile: null,
+    attributes: {
+      intelligence: 0,
+      dexterity: 12,
+      constitution: 0,
+      insanity: 10,
+      charisma: 12,
+      strength: -19,
+    },
+    classroom_id: '',
+    tasks:[],
+    nickname: "Reik",
+    name: "Reik",
+    gold: 5750,
+    equipment: {
+      weapon: '',
+      armor: '',
+      artifact: '',
+      antidote_potion: '',
+      healing_potion: '',
+      enhancer_potion: '',
+      helmet: '',
+      shield: '',
+      boot: '',
+      ring: '',
+    },
+    inventory: {
+      helmets: [],
+      weapons: [],
+      armors: [],
+      shields: [
+        '66f27c81c114335cadf45d70',
+      ],
+      artifacts: [],
+      boots: [],
+      rings: [],
+      antidote_potions: [],
+      healing_potions: [],
+      enhancer_potions: [],
+      ingredients: [
+        '6702b39d76863c206a48cccb',
+        '6702b39d76863c206a48cccb',
+        '6702b39d76863c206a48cccb',
+        '6702b39d76863c206a48cccb',
+        '6702b39d76863c206a48cccb',
+        '6702b39d76863c206a48cccb',
+        '6702b39d76863c206a48cccb',
+        '6702b39d76863c206a48cccb',
+      ],
+    },
+    level: 17,
 };
 
 const mockCart: Cart = [{
--- src/_common/interfaces/UnPopulatedPlayer.ts
+++ src/_common/interfaces/UnPopulatedPlayer.ts
@@ -3,43 +3,43 @@ import { Modifier } from "./Modifier";
 import { Task } from "./Task";
 
 export interface UnPopulatedPlayer {
-  _id?: string;
-  name?: string;
-  nickname?: string;
-  avatar?: string;
-  email?: string;
-  experience?: number;
-  level?: number;
-  gold?: number;
-  is_active?: boolean;
-  created_date?: string;
-  profile?: Profile;
-  attributes?: Modifier;
-  classroom_id?: string;
-  equipment?: {
-    helmet?: string;
-    weapon?: string;
-    armor?: string;
-    shield?: string;
-    artifact?: string;
-    boot?: string;
-    ring?: string;
-    healing_potion?: string;
-    antidote_potion?: string;
-    enhancer_potion?: string;
-  };
-  inventory?: {
-    helmets?: string[];
-    weapons?: string[];
-    armors?: string[];
-    shields?: string[];
-    artifacts?: string[];
-    boots?: string[];
-    rings?: string[];
-    healing_potions?: string[];
-    antidote_potions?: string[];
-    enhancer_potions?: string[];
-    ingredients?: string[];
-  };
-  tasks?: Task[];
+  _id: string;
+  name: string;
+  nickname: string;
+  avatar: string;
+  email: string;
+  experience: number;
+  level: number;
+  gold: number;
+  is_active: boolean;
+  created_date: string;
+  profile: Profile | null;
+  attributes: Modifier;
+  classroom_id: string | null;
+  equipment:{
+    helmet: string,
+    weapon: string,
+    armor: string,
+    shield: string,
+    artifact: string,
+    boot: string,
+    ring: string,
+    healing_potion: string,
+    antidote_potion: string,
+    enhancer_potion: string,
+  },
+  inventory:{
+    helmets: string[],
+    weapons: string[],
+    armors: string[],
+    shields: string[],
+    artifacts: string[],
+    boots: string[],
+    rings: string[],
+    healing_potions: string[],
+    antidote_potions: string[],
+    enhancer_potions: string[],
+    ingredients: string[],
+  },
+  tasks: Task[]
 }
